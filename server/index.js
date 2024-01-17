e4ca046fcc9af831fb1e5c6db2681bd52726afa7
Cecilia urrutia <urrutia.cici@gmail.com>
2024-01-17 18:10:39 -0300

    subo proyecto sin database, solo inicio

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+
+
+const port = process.env.PORT ?? 3000 
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket)=> {
+    console.log('A user has connected!')
+
+    socket.on('disconnect', ()=> {
+        console.log('A user has disconnected')
+    })
+
+    socket.on('chat message', (msg) => {
+        //console.log('mensaje', msg)
+       io.emit('chat message', msg )
+    })
+    
+
+})
+
+
+
+app.use(logger('dev'))
+
+app.get ('/', (reg, res) => {res.sendFile(process.cwd() + '/client/index.html')})
+
+server.listen(port, ()=> {
+    console.log(`Server running on port $(port`)
+})
+
